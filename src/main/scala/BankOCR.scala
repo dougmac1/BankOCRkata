ca60ca39c3e64edb08d842a157817ec32ac8d39b
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-03-26 13:29:49 +0100

    changed to lists for parsing number

--- src/main/scala/BankOCR.scala
+++ src/main/scala/BankOCR.scala
@@ -1,12 +1,68 @@
 object BankOCR extends App {
 
-  def convert(x : String) : Int = {
+  def convert(x: String): Int = {
 
-    if (x == """
-          |
-          |
+    val numList: Map[String, Int] = Map(
+      List(
+        "   ",
+        "  |",
+        "  |",
+        "   ").mkString("\n") -> 1,
+      List(
+        " _ ",
+        " _|",
+        "|_ ",
+        "   ").mkString("\n") -> 2,
+      List(
+        " _ ",
+        " _|",
+        " _|",
+        "   ").mkString("\n") -> 3,
+      List(
+        "   ",
+        "|_|",
+        "  |",
+        "   ").mkString("\n") -> 4,
+      List(
+        " _ ",
+        "|_ ",
+        " _|",
+        "   ").mkString("\n") -> 5,
+      List(
+        " _ ",
+        "|_ ",
+        "|_|",
+        "   ").mkString("\n") -> 6,
+      List(
+        " _ ",
+        "  |",
+        "  |",
+        "   ").mkString("\n") -> 7,
+      List(
+        " _ ",
+        "|_|",
+        "|_|",
+        "   ").mkString("\n") -> 8,
+      List(
+        " _ ",
+        "|_|",
+        " _|",
+        "   ").mkString("\n") -> 9,
+      List(
+        " _ ",
+        "| |",
+        "|_|",
+        "   ").mkString("\n") -> 0)
 
-        """.stripMargin) 1 else 2
+    numList(x)
+
   }
+
+  println(convert("""
+         _
+        |_|
+         _|
+        """.stripMargin))
+
 
 }
--- src/test/scala/BankOCRSpec.scala
+++ src/test/scala/BankOCRSpec.scala
@@ -5,34 +5,29 @@ class BankOCRSpec extends WordSpec with MustMatchers {
   "BankOCR" must {
 
     "return 1 when given two vertical pipes" in {
-      val input =
-        """
-          |
-          |
-
-        """.stripMargin
+      val input =List(
+        "   ",
+        "  |",
+        "  |",
+        "   ").mkString("\n")
       BankOCR.convert(input) mustEqual 1
     }
 
     "return 2 when representing 2 as pipes" in {
-      val input =
-        """
-           _
-           _|
-          |_
-
-        """.stripMargin
+      val input =List(
+        " _ ",
+        " _|",
+        "|_ ",
+        "   ").mkString("\n")
       BankOCR.convert(input) mustEqual 2
     }
 
     "return 3 when representing 3 as pipes" in {
-      val input =
-        """
-           _
-           _|
-           _|
-
-        """.stripMargin
+      val input =List(
+        " _ ",
+        " _|",
+        " _|",
+        "   ").mkString("\n")
       BankOCR.convert(input) mustEqual 3
     }
   }
