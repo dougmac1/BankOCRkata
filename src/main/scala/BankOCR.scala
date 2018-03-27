3a5d2738db28fceb5e81def0db6ed195522a84db
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-03-27 10:31:11 +0100

    add for loop to deal with more numbers add test for multiple zeros

--- src/main/scala/BankOCR.scala
+++ src/main/scala/BankOCR.scala
@@ -1,6 +1,6 @@
 object BankOCR extends App {
 
-  def convert(input: String): Int = {
+  def convert(input: String): String = {
 
     val numList: Map[String, Int] = Map(
       List(
@@ -56,19 +56,21 @@ object BankOCR extends App {
 
     val split = input.split("\n").toList
 
-    def splitToCharBy3 (row: String): List[String] = {
-      for (grid <- 0 until 6 by 3) yield {
-        val chars = row.substring(grid, grid + 3)
-        chars
 
+      val numberCount = split.head.length / 3
+      val top = splitToCharBy3(split(0))
+      val middle = splitToCharBy3(split(1))
+      val bottom = splitToCharBy3(split(2))
+      val output = for (eachNum <- 0 until numberCount) yield {
+        numList(top(eachNum) + "\n" + middle(eachNum) + "\n" + bottom(eachNum) + "\n" + "   ")
       }
+      output.mkString
+
+
     }
 
-    val top = splitToCharBy3(split(0))
-    val middle = splitToCharBy3(split(1))
-    val bottom = splitToCharBy3(split(2))
+  def splitToCharBy3(row: String): List[String] = {
+    "(...)".r.findAllIn(row).toList
 
-
-
+    }
   }
-}
--- src/test/scala/BankOCRSpec.scala
+++ src/test/scala/BankOCRSpec.scala
@@ -10,7 +10,7 @@ class BankOCRSpec extends WordSpec with MustMatchers {
         "  |",
         "  |",
         "   ").mkString("\n")
-      BankOCR.convert(input) mustEqual 1
+      BankOCR.convert(input) mustEqual "1"
     }
 
     "return 2 when representing 2 as pipes" in {
@@ -19,7 +19,7 @@ class BankOCRSpec extends WordSpec with MustMatchers {
         " _|",
         "|_ ",
         "   ").mkString("\n")
-      BankOCR.convert(input) mustEqual 2
+      BankOCR.convert(input) mustEqual "2"
     }
 
     "return 3 when representing 3 as pipes" in {
@@ -28,7 +28,7 @@ class BankOCRSpec extends WordSpec with MustMatchers {
         " _|",
         " _|",
         "   ").mkString("\n")
-      BankOCR.convert(input) mustEqual 3
+      BankOCR.convert(input) mustEqual "3"
     }
 
     "return 12 when given 12 as pipes" in {
@@ -37,7 +37,16 @@ class BankOCRSpec extends WordSpec with MustMatchers {
         "  | _|",
         "  ||_ ",
         "      ").mkString("\n")
-      BankOCR.convert(input) mustEqual 12
+      BankOCR.convert(input) mustEqual "12"
+    }
+
+    "return 000000000 when given nine 0's as pipes" in {
+      val input = List(
+          " _  _  _  _  _  _  _  _  _ ",
+          "| || || || || || || || || |",
+          "|_||_||_||_||_||_||_||_||_|",
+          "                           ").mkString("\n")
+      BankOCR.convert(input) mustEqual "000000000"
     }
   }
 }
