e7a5891bfb8a34ff14f1eaf904d0f07761555e66
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-03-28 23:32:33 +0100

    working example for all user stories

--- src/main/scala/BankOCR.scala
+++ src/main/scala/BankOCR.scala
@@ -2,9 +2,9 @@ import java.io.{BufferedWriter, File, FileWriter}
 import scala.util.Try
 import scala.io.Source
 
-object ReadFile {
+class ReadFile {
   def read(name : String) = {
-    val buffer = Source.fromFile(s"/home/digital030585/TrainingFiles/BankOCRkata/src/main/scala/$name")
+    val buffer = Source.fromFile(s"./src/main/scala/$name")
     val fileContents = buffer.getLines().map(line =>
       line + "\n"
     ).mkString
@@ -13,10 +13,10 @@ object ReadFile {
   }
 }
 
-object WriteFile {
+class WriteFile {
 
   def write(text: String) : File = {
-    val file = new File("/home/digital030585/TrainingFiles/BankOCRkata/src/main/scala/Account Output.txt")
+    val file = new File("./src/main/scala/Account Output.txt")
     val bufferedWriter = new BufferedWriter(new FileWriter(file))
     bufferedWriter.write(text)
     bufferedWriter.close()
@@ -25,6 +25,63 @@ object WriteFile {
 }
 
 object BankOCR extends App {
+
+  val readFile = new ReadFile
+  val writeFile = new WriteFile
+
+  lazy val numList: Map[String, Int] = Map(
+
+    "   " +
+      "  |" +
+      "  |" +
+      "   " -> 1,
+
+    " _ " +
+      " _|" +
+      "|_ " +
+      "   " -> 2,
+
+    " _ " +
+      " _|" +
+      " _|" +
+      "   " -> 3,
+
+    "   " +
+      "|_|" +
+      "  |" +
+      "   " -> 4,
+
+    " _ " +
+      "|_ " +
+      " _|" +
+      "   " -> 5,
+
+    " _ " +
+      "|_ " +
+      "|_|" +
+      "   " -> 6,
+
+    " _ " +
+      "  |" +
+      "  |" +
+      "   " -> 7,
+
+    " _ " +
+      "|_|" +
+      "|_|" +
+      "   " -> 8,
+
+    " _ " +
+      "|_|" +
+      " _|" +
+      "   " -> 9,
+
+    " _ " +
+      "| |" +
+      "|_|" +
+      "   " -> 0)
+
+  lazy val numMap = numList.map(_.swap)
 
   def parseAccounts(input: String): List[String] = {
     "(.+\n?){4}".r.findAllIn(input).toList
@@ -45,62 +102,11 @@ object BankOCR extends App {
   def convert(input: String): String = {
     val parsedAccs = parseAccounts(input)
     val parsed = parsedAccs.map(x => parseDigits(x))
-    val converted = parsed.map(x => convertToNumber(x))
-    converted.map(x => checkSum(x)).mkString("\n")
+    println(parsed.map(x => guessNumbers(x)).mkString("\n"))
+    parsed.map(x => guessNumbers(x)).mkString("\n")
   }
 
   def convertToNumber(listOfDigits: List[String]): String = {
-    val numList: Map[String, Int] = Map(
-
-        "   " +
-        "  |" +
-        "  |" +
-        "   " -> 1,
-
-        " _ " +
-        " _|" +
-        "|_ " +
-        "   " -> 2,
-
-        " _ " +
-        " _|" +
-        " _|" +
-        "   " -> 3,
-
-        "   " +
-        "|_|" +
-        "  |" +
-        "   " -> 4,
-
-        " _ " +
-        "|_ " +
-        " _|" +
-        "   " -> 5,
-
-        " _ " +
-        "|_ " +
-        "|_|" +
-        "   " -> 6,
-
-        " _ " +
-        "  |" +
-        "  |" +
-        "   " -> 7,
-
-        " _ " +
-        "|_|" +
-        "|_|" +
-        "   " -> 8,
-
-        " _ " +
-        "|_|" +
-        " _|" +
-        "   " -> 9,
-
-        " _ " +
-        "| |" +
-        "|_|" +
-        "   " -> 0)
     listOfDigits.map(digit => Try(numList(digit)) getOrElse "?").mkString
   }
 
@@ -117,5 +123,35 @@ object BankOCR extends App {
       }
     }
   }
-  WriteFile.write(convert(ReadFile.read("Bank Account Numbers.txt")))
+
+  def guessNumbers(input: List[String]): String = {
+    val allCombinations = for (eachNum <- input.indices.toList) yield {
+      val numberPatterns = (0 to 9).toList.map(numMap)
+      val compareWithInput = numberPatterns.map(x => input(eachNum).zip(x))
+      val numOfChanges = compareWithInput.map(x => x.map(l => if (l._2 != l._1) 1 else 0).sum)
+      val addNumbers = numOfChanges.zipWithIndex
+      val getOneChangeOrLess = addNumbers.filter(x => x._1 <= 1)
+      val returnMatches = getOneChangeOrLess.map(_._2.toString)
+      returnMatches
+    }
+
+    val possibleAccounts = getAccountCombinations(allCombinations).map(_.mkString)
+    val comparison = convertToNumber(input)
+    val validOptions = possibleAccounts.filter(checkSum(_).length == 9)
+    val compareChanges = validOptions.map(x => x.zip(comparison))
+    val filteredForOneChange = compareChanges.filter(x => x.count(x => x._2 != x._1) ==1 )
+    val validNumbers = filteredForOneChange.map(x => x.map(x => x._1).mkString)
+    validNumbers.length match {
+      case 0 => checkSum(comparison)
+      case 1 => validNumbers.head
+      case _ => s"$comparison AMB ['${validNumbers.mkString("', '")}']"
+    }
+  }
+
+  def getAccountCombinations(inputList: List[List[String]]): List[List[String]] = inputList match {
+    case Nil => List(Nil)
+    case h :: _ => h.flatMap(i => getAccountCombinations(inputList.tail).map(i :: _))
+  }
+
+  writeFile.write(convert(readFile.read("Bank Account Numbers.txt")))
 }
--- src/main/scala/banktest.sc
+++
@@ -1,7 +0,0 @@
-def splitToCharBy3 (row: String): IndexedSeq[String] = {
-  for (grid <- 0 until 6 by 3) yield {
-    val chars = row.substring(grid, grid + 3)
-    chars
-  }
-
-}
